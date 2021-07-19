c005d93c1e88485cf47ed2e5b387dd1d11364bfc
Kipelov99 <jeka949494@tut.by>
2021-07-19 12:53:41 +0300

    Add FileCabinetService.cs file.

--- FileCabinetApp/FileCabinetRecord.cs
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -1,15 +1,12 @@
 using System;
 
-namespace FileCabinetService
+public class FileCabinetRecord
 {
-    public class FileCabinetRecord
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string FirstName { get; set; }
+    public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+    public string LastName { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
-    }
+    public DateTime DateOfBirth { get; set; }
 }
